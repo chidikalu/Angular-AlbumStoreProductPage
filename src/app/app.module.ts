9391e89d465532eb17691eaf50aa5717eeb011a0
chidikalu <40118011+chidikalu@users.noreply.github.com>
2020-10-09 23:48:14 +0100

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,15 @@ import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
+
+import { RouterModule, Routes } from "@angular/router";
+
+const appRoutes: Routes = [
+  { path: 'products', component: ProductListComponent },
+  { path: 'product/:id', component: ProductPageComponent },
+  { path: '', redirectTo: 'products', pathMatch: 'full' }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,6 +32,7 @@ import { ProductListComponent } from './product-list/product-list.component';
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
